78fc5d7a2ae11bbd28483ac251171ce608bcb300
MykhailoVas <lucky_1985@ukr.net>
2019-08-05 23:25:07 +0300

    Home work 1

---
+++ HomeWork1_1.java
@@ -0,0 +1,10 @@
+
+public class HomeWork1_1 {
+
+	public static void main(String[] args) {
+		int radius = 5;
+		System.out.println("Perimetr of circle is "+(2 * Math.PI * radius));
+		System.out.println("Area of circle is "+(Math.PI * Math.pow(radius, 2)));
+		
+	}
+}
